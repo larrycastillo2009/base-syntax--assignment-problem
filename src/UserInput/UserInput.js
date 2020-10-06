f3c8bf5803bdbb7863abe66118ba3db0d20a8c9b
Larry Castillo <larrycastillo2009@gmail.com>
2020-10-05 22:15:48 -0500

    creation of the file tree

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const userInput = (props) => {
+    return (
+        <label for=userIn>User Input</label>
+        <input type=text id=userIn name=userIn>
+    )
+};
+
+export default person;
